b9591099dc7cb3f5400b261a3f253d40b80857f3
Ivan Ibarra <ivanibarra742@gmail.com>
2025-03-24 23:00:27 -0600

    24/03/2025

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -1,12 +1,16 @@
 namespace API.Data;
 
 using System.Collections.Generic;
+using System.Globalization;
 using System.Threading.Tasks;
 using API.DataEntities;
 using API.DTOs;
 using API.Helpers;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
 
-public class MessageRepository(DataContext context) : IMessageRepository
+public class MessageRepository(DataContext context, IMapper mapper) : IMessageRepository
 {
     public void Add(Message message) => context.Messages.Add(message);
 
@@ -14,14 +18,48 @@ public class MessageRepository(DataContext context) : IMessageRepository
 
     public async Task<Message?> GetAsync(int id) => await context.Messages.FindAsync(id);
 
-    public Task<PagedList<MessageResponse>> GetForUserAsync()
+    public async Task<PagedList<MessageResponse>> GetForUserAsync(MessageParams messageParams)
     {
-        throw new NotImplementedException();
+        var query = context.Messages.OrderByDescending(m => m.MessageSent)
+            .AsQueryable();
+
+        query = messageParams.Container.ToLower(CultureInfo.InvariantCulture) switch
+        {
+            "inbox" => query.Where(m => m.Recipient.UserName == messageParams.Username),
+            "outbox" => query.Where(m => m.Sender.UserName == messageParams.Username),
+            _ => query.Where(m => m.Recipient.UserName == messageParams.Username
+                && m.DateRead == null)
+        };
+
+        var messages = query.ProjectTo<MessageResponse>(mapper.ConfigurationProvider);
+
+        return await PagedList<MessageResponse>
+            .CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
     }
 
-    public Task<IEnumerable<MessageResponse>> GetThreadAsync(string currentUsername, string recipientUsername)
+    public async Task<IEnumerable<MessageResponse>> GetThreadAsync(string currentUsername, string recipientUsername)
     {
-        throw new NotImplementedException();
+        var messages = await context.Messages
+            .Include(m => m.Sender).ThenInclude(p => p.Photos)
+            .Include(m => m.Recipient).ThenInclude(p => p.Photos)
+            .Where(m =>
+                (m.RecipientUsername == currentUsername && m.SenderUsername == recipientUsername) ||
+                (m.RecipientUsername == recipientUsername && m.SenderUsername == currentUsername)
+            )
+            .OrderBy(m => m.MessageSent)
+            .ToListAsync();
+
+        var unreadMessages = messages
+            .Where(m => m.DateRead == null && m.RecipientUsername == currentUsername)
+            .ToList();
+
+        if (unreadMessages.Count != 0)
+        {
+            unreadMessages.ForEach(m => m.DateRead = DateTime.UtcNow);
+            await context.SaveChangesAsync();
+        }
+
+        return mapper.Map<IEnumerable<MessageResponse>>(messages);
     }
 
     public async Task<bool> SaveAllAsync() => await context.SaveChangesAsync() > 0;
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MessagesService } from '../_services/messages.service';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { FormsModule } from '@angular/forms';
+import { TimeagoModule } from 'ngx-timeago';
+import { Message } from '../_models/message';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [],
+  imports: [ButtonsModule, FormsModule, TimeagoModule, RouterModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit {
+  messagesService = inject(MessagesService);
+  container = "unread";
+  pageNumber = 1;
+  pageSize = 10;
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.messagesService.getMessages(this.pageNumber, this.pageSize, this.container);
+  }
+
+  getRoute(message: Message) {
+  // if (this.container === "outbox") return `/members/${message.recipientUsername}`;
+  // else return `/members/${message.senderUsername}`;
+
+  return this.container === "outbox"
+    ? `/members/${message.recipientUsername}`
+    : `/members/${message.senderUsername}`;
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
 
 }
