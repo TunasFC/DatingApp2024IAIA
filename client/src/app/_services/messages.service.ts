b1fb3f89ec6fd6ff673956c0f97de83bb3ad2e60
Ivan Ibarra <ivanibarra742@gmail.com>
2025-04-11 19:10:32 -0600

    04/04/2025

--- client/src/app/_services/messages.service.ts
+++ client/src/app/_services/messages.service.ts
@@ -15,11 +15,23 @@ export class MessagesService {
 
   getMessages(pageNumber: number, pageSize: number, container: string) {
     let params = setPaginationHeaders(pageNumber, pageSize);
-    params = params.append("Container", container);
+    params = params.append("Container", container.toLocaleLowerCase());
 
     return this.http.get<Message[]>(this.baseUrl + "messages",
       { observe: "response", params }).subscribe({
         next: response => setPaginationResponse(response, this.paginatedResult)
       });
+  }
+
+  getMessageThread(username: string) {
+    return this.http.get<Message[]>(this.baseUrl + "messages/thread/" + username);
+  }
+
+  sendMessage(username: string, content: string) {
+    return this.http.post<Message>(this.baseUrl + "messages", { recipientUsername: username, content });
+  }
+
+  deleteMessage(id: number) {
+    return this.http.delete(this.baseUrl + "messages/" + id);
   }
 }
