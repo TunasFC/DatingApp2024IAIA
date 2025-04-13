fc7fba1e7981fadb3bb006ae153595a318f1d414
Ivan Ibarra <ivanibarra742@gmail.com>
2025-04-13 17:19:43 -0600

    ultimo

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -5,35 +5,51 @@ import { FormsModule } from '@angular/forms';
 import { TimeagoModule } from 'ngx-timeago';
 import { Message } from '../_models/message';
 import { RouterModule } from '@angular/router';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
-  imports: [ButtonsModule, FormsModule, TimeagoModule, RouterModule],
+  imports: [ButtonsModule, FormsModule, TimeagoModule, RouterModule, PaginationModule],
   templateUrl: './messages.component.html',
   styleUrl: './messages.component.css'
 })
 export class MessagesComponent implements OnInit {
   messagesService = inject(MessagesService);
-  container = "unread";
+  container = "Inbox";
   pageNumber = 1;
-  pageSize = 10;
+  pageSize = 5;
+  isOutbox = this.container === "Outbox";
 
   ngOnInit(): void {
     this.loadMessages();
   }
 
   loadMessages() {
-    this.messagesService.getMessages(this.pageNumber, this.pageSize, this.container);
+    this.messagesService.getMessages(this.pageNumber, this.pageSize, this.container.toLocaleLowerCase());
+  }
+
+  deleteMessage(id: number) {
+    this.messagesService.deleteMessage(id).subscribe({
+      next: _ => {
+        this.messagesService.paginatedResult.update(prev => {
+          if (prev && prev.items) {
+            prev.items.splice(prev.items.findIndex(m => m.id == id), 1);
+            return prev;
+          }
+          return prev;
+        });
+      }
+    });
   }
 
   getRoute(message: Message) {
-  // if (this.container === "outbox") return `/members/${message.recipientUsername}`;
-  // else return `/members/${message.senderUsername}`;
-
-  return this.container === "outbox"
-    ? `/members/${message.recipientUsername}`
-    : `/members/${message.senderUsername}`;
+    if (this.isOutbox) {
+      return `/members/${message.recipientUsername}`;
+    }
+    else {
+      return `/members/${message.senderUsername}`;
+    }
   }
 
   pageChanged(event: any) {
