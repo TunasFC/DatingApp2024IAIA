067d6195fe8a090a1dc84a71dd67c267301a7a7e
Ivan Ibarra <ivanibarra742@gmail.com>
2025-06-14 14:51:20 -0600

    14/06/2025
    comit 3

--- API/DataEntities/AppUser.cs
+++ API/DataEntities/AppUser.cs
@@ -1,13 +1,9 @@
 namespace API.DataEntities;
 
-using API.Extensions;
+using Microsoft.AspNetCore.Identity;
 
-public class AppUser
+public class AppUser : IdentityUser<int>
 {
-    public int Id { get; set; }
-    public required string UserName { get; set; }
-    public byte[] PasswordHash { get; set; } = [];
-    public byte[] PasswordSalt { get; set; } = [];
     public DateOnly BirthDay { get; set; }
     public required string KnownAs { get; set; }
     public DateTime Created { get; set; } = DateTime.Now;
@@ -19,6 +15,9 @@ public class AppUser
     public required string City { get; set; }
     public required string Country { get; set; }
     public List<Photo> Photos { get; set; } = [];
-
-    // public int GetAge() => BirthDay.CalculateAge();
+    public List<UserLike> LikedByUsers { get; set; } = [];
+    public List<UserLike> LikedUsers { get; set; } = [];
+    public List<Message> MessagesSent { get; set; } = [];
+    public List<Message> MessagesRecieved { get; set; } = [];
+    public ICollection<AppUserRole> UserRoles { get; set; } = [];
 }
